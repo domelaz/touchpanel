c4249add76b31b5b86b7557c5e4b1c809aca485c
Антон Домолазов <anton@domelaz.ru>
2016-01-15 18:34:52 +0300

    Show full slide also

--- src/client.js
+++ src/client.js
@@ -2,6 +2,7 @@
   'use strict';
 
   let eventClick = 'click';
+  let slide = $('<img class="slide" src="img/00-02.png">');
 
   function selectSlide() {
     let slideThumbnail = $(this);
@@ -10,8 +11,19 @@
     slideThumbnail.addClass('thumbnail--active');
   }
 
+  function showSlide() {
+    if ($('.slide').length === 0) {
+      $('.content').append(slide);
+    }
+    slide.attr({
+      src: $(this).attr('src')
+    });
+  }
+
   $(function() {
-    $('.thumbnails-set').on(eventClick, ".thumbnail", selectSlide);
+    $('.thumbnails-set')
+      .on(eventClick, ".thumbnail", selectSlide)
+      .on(eventClick, ".thumbnail", showSlide);
   });
 
 })(jQuery);
