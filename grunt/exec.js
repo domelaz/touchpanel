170a88c7262d59ef76f3a2149df733b95dc8e35b
Антон Домолазов <anton@domelaz.ru>
2016-01-16 14:41:34 +0300

    Fix grunt exec:build on Win32
    На windows слэши в другую сторону и расширение файла нужно

--- grunt/exec.js
+++ grunt/exec.js
@@ -1,7 +1,37 @@
 'use strict';
 
+let path = require('path');
+
+/**
+ * Shortcut to platform
+ *
+ * @var {boolean}
+ */
+const WINDOWS = process.platform === 'win32';
+
+/**
+ * Step up, we use `load-grunt-config` and `pwd` here is 'grunt'
+ * @todo Move to Gruntfile.js `data`
+ */
+const projectRoot = '..';
+
+/**
+ * Resolve absolute path of executable, depending on platform
+ *
+ * @param {string} here Relative path to executable
+ * @param {string} name Platform independant executable name
+ * @param {string} ext  File extension for Win32
+ *
+ * @return {string}     Absolute executable path
+ */
+function getExecutable(here, name, ext) {
+  let execExt  = WINDOWS ? `.${ext}` : '';
+  let execFull = path.join(__dirname, projectRoot, here, name + execExt);
+  return execFull;
+}
+
 let build = [
-  'node_modules/.bin/electron-packager', // Packager executable
+  getExecutable('node_modules/.bin', 'electron-packager', 'cmd'),
   'app/', // Source dir
   'Nord-Interactive', // App name
   '--platform=' + process.platform,
