5fd170a2dcd166d05452eb25942b2b09b2c02f61
Антон Домолазов <anton@domelaz.ru>
2016-01-27 16:15:16 +0300

    Empty action added

---
+++ src/actions/index.tsx
@@ -0,0 +1,7 @@
+export const THUMB_CLICK = 'THUMB_CLICK';
+
+export function thumbClick() {
+  return {
+    type: THUMB_CLICK
+  };
+}
--- src/reducers/index.tsx
+++ src/reducers/index.tsx
@@ -1,6 +1,8 @@
+import { THUMB_CLICK } from '../actions';
+
 export default function baggage(state = 0, action) {
   switch (action.type) {
-    case 'THUMB_CLICK':
+    case THUMB_CLICK:
       return Object.assign({}, state);
     default:
       return state;
