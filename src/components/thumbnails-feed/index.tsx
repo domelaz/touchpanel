315a3aedfaab55a23d2bafb4271ec3b33145f2b7
Антон Домолазов <anton@domelaz.ru>
2016-01-27 19:36:20 +0300

    Get rid of ThumbnailsSet component

--- src/components/thumbnails-feed/index.tsx
+++ src/components/thumbnails-feed/index.tsx
@@ -3,14 +3,21 @@
 'use strict';
 
 import * as React from 'react';
-import ThumbnailsSet from './ThumbnailsSet';
+import Thumbnail from '../thumbnail';
 
 export default class ThumbnailsFeed extends React.Component<{}, {}> {
   render() {
     return (
       <div className="thumbnails-feed">
         <div className="thumbnails-nav"></div>
-        <ThumbnailsSet />
+          <div className="thumbnails-set">
+            <Thumbnail src="img/00-02.png"/>
+            <Thumbnail src="img/00-03.png"/>
+            <Thumbnail src="img/00-04.png" cls="thumbnail thumbnail--active"/>
+            <Thumbnail src="img/00-05.png"/>
+            <Thumbnail src="img/00-06.png"/>
+            <Thumbnail src="img/00-07.png"/>
+          </div>
         <div className="thumbnails-nav thumbnails-nav--right"></div>
       </div>
     );
