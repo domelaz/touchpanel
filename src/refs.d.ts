7700c77a428b7fb957d57a005e757b6454bb1d6a
Антон Домолазов <anton@domelaz.ru>
2016-01-27 14:55:28 +0300

    Redux and react-redux added

--- src/refs.d.ts
+++ src/refs.d.ts
@@ -5537,3 +5537,124 @@ declare module "react-dom/server" {
     import DOMServer = __React.__DOMServer;
     export = DOMServer;
 }
+// Type definitions for Redux v1.0.0
+// Project: https://github.com/rackt/redux
+// Definitions by: William Buchwalter <https://github.com/wbuchwalter/>, Vincent Prouillet <https://github.com/Keats/>
+// Definitions: https://github.com/borisyankov/DefinitelyTyped
+
+declare module Redux {
+
+    interface ActionCreator extends Function {
+        (...args: any[]): any;
+    }
+
+    interface Reducer extends Function {
+        (state: any, action: any): any;
+    }
+
+    interface Dispatch extends Function {
+        (action: any): any;
+    }
+
+    interface StoreMethods {
+        dispatch: Dispatch;
+        getState(): any;
+    }
+
+
+    interface MiddlewareArg {
+        dispatch: Dispatch;
+        getState: Function;
+    }
+
+    interface Middleware extends Function {
+        (obj: MiddlewareArg): Function;
+    }
+
+    class Store {
+        getReducer(): Reducer;
+        replaceReducer(nextReducer: Reducer): void;
+        dispatch(action: any): any;
+        getState(): any;
+        subscribe(listener: Function): Function;
+    }
+
+    function createStore(reducer: Reducer, initialState?: any): Store;
+    function bindActionCreators<T>(actionCreators: T, dispatch: Dispatch): T;
+    function combineReducers(reducers: any): Reducer;
+    function applyMiddleware(...middlewares: Middleware[]): Function;
+    function compose<T extends Function>(...functions: Function[]): T;
+}
+
+declare module "redux" {
+    export = Redux;
+}
+// Type definitions for react-redux 2.1.2
+// Project: https://github.com/rackt/react-redux
+// Definitions by: Qubo <https://github.com/tkqubo>
+// Definitions: https://github.com/borisyankov/DefinitelyTyped
+
+/// <reference path="../react/react.d.ts" />
+/// <reference path="../redux/redux.d.ts" />
+
+declare module "react-redux" {
+  import { Component } from 'react';
+  import { Store, Dispatch, ActionCreator } from 'redux';
+
+  export class ElementClass extends Component<any, any> { }
+  export interface ClassDecorator {
+    <T extends (typeof ElementClass)>(component: T): T
+  }
+
+  /**
+   * Connects a React component to a Redux store.
+   * @param mapStateToProps
+   * @param mapDispatchToProps
+   * @param mergeProps
+   * @param options
+     */
+  export function connect(mapStateToProps?: MapStateToProps,
+                          mapDispatchToProps?: MapDispatchToPropsFunction|MapDispatchToPropsObject,
+                          mergeProps?: MergeProps,
+                          options?: Options): ClassDecorator;
+
+  interface MapStateToProps {
+    (state: any, ownProps?: any): any;
+  }
+
+  interface MapDispatchToPropsFunction {
+    (dispatch: Dispatch, ownProps?: any): any;
+  }
+
+  interface MapDispatchToPropsObject {
+    [name: string]: ActionCreator;
+  }
+
+  interface MergeProps {
+    (stateProps: any, dispatchProps: any, ownProps: any): any;
+  }
+
+  interface Options {
+    /**
+     * If true, implements shouldComponentUpdate and shallowly compares the result of mergeProps,
+     * preventing unnecessary updates, assuming that the component is a “pure” component
+     * and does not rely on any input or state other than its props and the selected Redux store’s state.
+     * Defaults to true.
+     * @default true
+     */
+    pure: boolean;
+  }
+
+  export interface Property {
+    /**
+     * The single Redux store in your application.
+     */
+    store?: Store;
+    children?: Function;
+  }
+
+  /**
+   * Makes the Redux store available to the connect() calls in the component hierarchy below.
+   */
+  export class Provider extends Component<Property, {}> { }
+}
