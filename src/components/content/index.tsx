67b3493214d2d5415836e16dd37dce72dd334cf0
Антон Домолазов <anton@domelaz.ru>
2016-01-29 18:26:33 +0300

    Animate content with React transitions

--- src/components/content/index.tsx
+++ src/components/content/index.tsx
@@ -3,6 +3,7 @@
 'use strict';
 
 import * as React from 'react';
+import * as ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 
 interface IContent {
   active?: number
@@ -12,9 +13,14 @@ export default class Content extends React.Component<IContent, {}> {
   render() {
     const { active } = this.props;
     return (
-      <div className="content">
-        <img className="slide" src={`img/00-0${active}.png`} />
-      </div>
+      <ReactCSSTransitionGroup
+        transitionName="content"
+        transitionEnterTimeout={500}
+        transitionLeaveTimeout={300}>
+          <div key={active} className="content">
+            <img className="slide" src={`img/00-0${active}.png`} />
+          </div>
+      </ReactCSSTransitionGroup>
     );
   }
 }
