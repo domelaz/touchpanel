ae0a3a24967f7c65370d1e83145354a8afa7123e
Антон Домолазов <anton@domelaz.ru>
2016-01-29 18:15:16 +0300

    Get rid of JQuery

--- src/components/thumbnails-feed/index.tsx
+++ src/components/thumbnails-feed/index.tsx
@@ -2,8 +2,6 @@
 
 'use strict';
 
-import * as $ from 'jquery';
-
 import * as React from 'react';
 import Thumbnail from '../thumbnail';
 
@@ -13,36 +11,7 @@ interface IFeedProps {
   onThumbnailClick(number): void
 }
 
-/**
-let eventClick = 'click';
-let slide = $('<img class="slide">');
-
-function showSlide() {
-  if ($('.slide').length === 0) {
-    $('.content').append(slide);
-  }
-  let newSrc = $(this).attr('src');
-  slide.animate({
-    opacity: 0
-  }, 200, function() {
-    slide.attr({
-      src: newSrc
-    });
-    slide.animate({
-      opacity: 1
-    }, 200);
-  });
-}*/
-
 export default class ThumbnailsFeed extends React.Component<IFeedProps, {}> {
-/**  componentDidMount() {
-    $('.thumbnails-set')
-      .on(eventClick, ".thumbnail", showSlide);
-  }
-  componentWillUnmount() {
-    $('.thumbnails-set').off();
-  }*/
-
   render() {
     const { thumbnails, active } = this.props;
 
